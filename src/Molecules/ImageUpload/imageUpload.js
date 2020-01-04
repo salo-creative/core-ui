82862b413faf9c70615895734b9cc4125bba2be4
Rich Comber <rich@elementalwebdesign.co.uk>
2020-01-04 13:51:25 +0000

    Changing loader appearance

--- src/Molecules/ImageUpload/imageUpload.js
+++ src/Molecules/ImageUpload/imageUpload.js
@@ -42,7 +42,7 @@ const ImageUpload = ({
         loaderProps={ {
           position: 'absolute'
         } }
-        theme='white'
+        appearance='light'
       />
       { !loading
           && (
